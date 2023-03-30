55c9ff64c5e39a73f6206aef9ad8c2dacc69a4bf
niexinquan <3290937695@qq.com>
2023-03-30 21:31:19 +0800

    test ql

---
+++ testQL.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+        System.out.println("Hello world!");
+    }
+}
